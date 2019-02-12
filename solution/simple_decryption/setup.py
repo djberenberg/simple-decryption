9a9def1f676520f48498be8c9996c9e89cb512f4
danielberenberg <djberenb@uvm.edu>
2019-02-12 11:45:33 -0500

    solution

---
+++ solution/simple_decryption/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(name="simple_decryption",
+                 version="1.0",
+                 author="Daniel Berenberg",
+                 description="Decrypt ciphertext",
+                 packages=setuptools.find_packages()
+                 )
